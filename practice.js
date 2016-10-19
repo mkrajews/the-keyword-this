d0753f641e3313f616868af40b375530508adeea
mkrajews <mimikrajewski@Jakes-MBP-2.home>
2016-10-18 22:32:34 -0600

    almost completed the-keyword-this project commit

--- practice.js
+++ practice.js
@@ -2,18 +2,26 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
+// the this keyword refers back to an object giving us an alternative and more specific way of accessing information
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
+      // explicit
+      // implicit
+      // default/window 
+        // new
 
   // 3) What is the difference between call and apply?
 
       //Answer
+      // call - not array
+      // apply -array
 
   // 4) What does .bind do?
 
       //Answer
+      // merges object and function to create new function for later access (without changing original function)
 
 
 //Next Problem
@@ -24,7 +32,15 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
+    var user = {
+      username: "name",
+      email: "email",
+      getUsername: function() {
+        return this.username;
+      }
 
+    }
+getUsername();
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
 
@@ -32,6 +48,16 @@
 
 
 // Write the function definitions which will make the following function invocations function properly.
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function() {
+    this.move += 10;
+  return this.move;
+}
+}
 
   //Function Invocations Here
 
@@ -74,9 +100,11 @@ var userName = getMyUsername(); //Fix this
 //Above you're given an object, and  a function. What will the getUsername function return?
 //Note(no tests)
   //Answer Here
+  // undefined
 
 //In the example above, what is the 'this keyword' bound to when getUsername runs?
 
   //Answer Here
+  // isn't bound to anything
 
 //Fix the getMyUsername invocation so that userName will be equal to 'iliketurtles'.
